6183720afb16bb89a4252e33c9a5e88797dbb141
sycoqz <leo.khlystov@gmail.com>
2023-12-03 02:29:00 +0300

    db show_fk

--- public_html/core/admin/controllers/AddController.php
+++ public_html/core/admin/controllers/AddController.php
@@ -2,6 +2,9 @@
 
 namespace core\admin\controllers;
 
+use core\base\exceptions\DbException;
+use core\base\settings\Settings;
+
 class AddController extends BaseAdmin
 {
 
@@ -11,7 +14,102 @@ class AddController extends BaseAdmin
 
         $this->createTableData();
 
+        $this->createForeignData();
+
         $this->createOutputData();
+
+    }
+
+    protected function createForeignProperty(array $arr, array $rootItems): void
+    {
+
+        $where = '';
+        $operand = [];
+
+        if (in_array($this->table, $rootItems['tables'])) {
+            $this->foreignData[$arr['COLUMN_NAME']][0]['id'] = 0;
+            $this->foreignData[$arr['COLUMN_NAME']][0]['name'] = $rootItems['name'];
+        }
+
+        $columns = $this->model->showColumns($arr['REFERENCED_TABLE_NAME']);
+
+        $name = '';
+
+        if (isset($columns['name'])) {
+            $name = 'name';
+        } else {
+            foreach ($columns as $key => $value) {
+
+                if (str_contains($key, 'name')) {
+                    $name = $key . ' as name';
+                }
+            }
+
+            if (!isset($name)) $name = $columns['id_row'] . ' as name';
+
+        }
+
+        if (!empty($this->data)) {
+
+            if ($arr['REFERENCED_TABLE_NAME'] === $this->table) {
+                // Значение станет строкой в $where
+                $where[$this->columns['id_row']] = $this->data[$this->columns['id_row']];
+                $operand[] = '<>';
+
+            }
+
+        }
+
+        $foreign = $this->model->read($arr['REFERENCED_TABLE_NAME'], [
+            'fields' => [$arr['REFERENCED_COLUMN_NAME'] . ' as id', $name],
+            'where' => $where,
+            'operand' => $operand
+        ]);
+
+        if (isset($foreign)) {
+
+            if (isset($this->foreignData[$arr['COLUMN_NAME']])) {
+
+                foreach ($foreign as $value) {
+                    $this->foreignData[$arr['COLUMN_NAME']][] = $value;
+                }
+            } else {
+                $this->foreignData[$arr['COLUMN_NAME']] = $foreign;
+            }
+
+        }
+
+    }
+
+    /**
+     * @throws DbException
+     */
+    protected function createForeignData(bool $settings = false): void
+    {
+
+        if (!$settings) $settings = Settings::instance();
+
+        $rootItems = $settings::get('rootItems');
+
+        $keys = $this->model->showForeignKeys($this->table);
+
+        if (isset($keys)) {
+
+            foreach ($keys as $item) {
+
+                $this->createForeignProperty($item, $rootItems);
+
+            }
+
+        } elseif (isset($this->columns['parent_id'])) {
+
+            $arr['COLUMN_NAME'] = 'parent_id';
+            $arr['REFERENCED_COLUMN_NAME'] = $this->columns['id_row'];
+            $arr['REFERENCED_TABLE_NAME'] = $this->table;
+
+            $this->createForeignProperty($arr, $rootItems);
+
+        }
 
     }
 }
--- public_html/core/admin/models/Model.php
+++ public_html/core/admin/models/Model.php
@@ -3,11 +3,33 @@
 namespace core\admin\models;
 
 use core\base\controllers\Singleton;
+use core\base\exceptions\DbException;
 use core\base\models\BaseModel;
+use JetBrains\PhpStorm\NoReturn;
 
 class Model extends BaseModel
 {
 
     use Singleton;
+
+    /**
+     * @throws DbException
+     */
+    #[NoReturn] public function showForeignKeys(string $table, bool $key = false): bool|array|int|string|null
+    {
+
+        $db = DB_NAME;
+        $where = '';
+
+        if ($key) $where = "AND COLUMN_NAME' = '$key' LIMIT 1";
+
+        $query = "SELECT COLUMN_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME 
+                    FROM information_schema.KEY_COLUMN_USAGE
+                      WHERE TABLE_SCHEMA = '$db' AND TABLE_NAME = '$table' AND
+                        CONSTRAINT_NAME <> 'PRIMARY' AND REFERENCED_TABLE_NAME is not null $where";
+
+        return $this->query($query);
+
+    }
 
 }
